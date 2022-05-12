a6c55c48dea69c0dae59979338046d7c0c24d84f
David Pineiro <david@davidpineiro.xyz>
2022-05-11 22:36:01 -0400

    added support for multiple chunks

--- GameCore/src/main/java/me/thatshawt/gameCore/game/Player.java
+++ GameCore/src/main/java/me/thatshawt/gameCore/game/Player.java
@@ -1,6 +1,9 @@
 package me.thatshawt.gameCore.game;
 
+import me.thatshawt.gameCore.tile.ChunkCoord;
+import me.thatshawt.gameCore.tile.ChunkMap;
 import me.thatshawt.gameCore.tile.PlayerTile;
+import me.thatshawt.gameCore.tile.TileChunk;
 
 import java.io.Serializable;
 import java.time.Clock;
@@ -12,15 +15,15 @@ public abstract class Player extends Entity implements Serializable{
     protected transient String chatMsg = "";
     private transient long chatTime;
 
-    public Player(int x, int y){
-        super(x,y,new PlayerTile());
+    public Player(ChunkMap chunks, int x, int y){
+        super(chunks, x,y,new PlayerTile());
     }
 
-    public Player(int x, int y, UUID uuid){
-        super(x,y,new PlayerTile(), uuid);
+    public Player(ChunkMap chunks, int x, int y, UUID uuid){
+        super(chunks, x,y,new PlayerTile(), uuid);
     }
 
-    public void setChat(String chatMsg){
+    public void setChat(String chatMsg) {
         this.chatMsg = chatMsg;
         this.chatTime = Clock.systemDefaultZone().millis();
     }
